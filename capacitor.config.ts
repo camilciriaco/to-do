4eee9ee171173c05cd14a9e2e119837e3c544b95
camilciriaco <camille.ciriaco@gmail.com>
2024-07-15 11:38:59 +0800

    change bundleId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ionic.demo',
   appName: 'To Do Task',
   webDir: 'www',
   server: {
